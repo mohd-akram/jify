1e11763402ca096d0930092207a426985c579ec8
Mohamed Akram <mohd.akram@outlook.com>
2018-08-18 03:31:27 +0400

    Update package

---
+++ test/rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from 'rollup-plugin-typescript3';
+
+export default {
+  input: 'index.ts',
+  output: {
+    format: 'cjs'
+  },
+  plugins: [
+    typescript({ compilerOptions: { target: 'ES2017' } })
+  ]
+};
