fa5d4a2f33ce204159257d7108378a80b915388e
Mohamed Akram <mohd.akram@outlook.com>
2021-07-12 20:07:11 +0400

    Use fs promises API

--- src/file.ts
+++ src/file.ts
@@ -1,17 +1,13 @@
 import { EventEmitter } from 'events';
-import * as fs from 'fs';
-import { promisify } from 'util';
+import { promises as fs } from 'fs';
+import { FileHandle } from 'fs/promises';
 
 import { lock, unlock } from 'os-lock';
 
 import { logger, read } from './utils';
 
-const fsOpen = promisify(fs.open);
-const fsClose = promisify(fs.close);
-const fsWrite = promisify(fs.write);
-
 class File extends EventEmitter {
-  protected fd: number | null = null;
+  protected file: FileHandle | null = null;
   protected lockedPositions =
     new Map<number, { exclusive: boolean, count: number }>();
   protected logger = logger('file');
@@ -24,75 +20,59 @@ class File extends EventEmitter {
   }
 
   get isOpen() {
-    return this.fd != null;
+    return this.file != null;
   }
 
   read(position: number, reverse = false, buffer?: Buffer) {
     ++this.reads;
-    if (!this.fd)
+    if (!this.file)
       throw new Error('Need to call open() before read()');
-    return read(this.fd, position, reverse, buffer);
+    return read(this.file, position, reverse, buffer);
   }
 
   async write(position: number, buffer: Buffer) {
     ++this.writes;
-    await fsWrite(this.fd!, buffer, 0, buffer.length, position);
+    await this.file!.write(buffer, 0, buffer.length, position);
   }
 
   async clear(position: number, length: number, char = ' ') {
     const buffer = Buffer.alloc(length, char);
-    await fsWrite(this.fd!, buffer, 0, length, position);
+    await this.file!.write(buffer, 0, length, position);
   }
 
   async truncate(position: number) {
-    await promisify(fs.ftruncate)(this.fd!, position);
-  }
-
-  async exists() {
-    return await promisify(fs.exists)(this.filename);
+    await this.file!.truncate(position);
   }
 
   async append(text: string) {
-    await promisify(fs.appendFile)(this.filename, text);
+    await fs.appendFile(this.filename, text);
   }
 
   async delete() {
-    await promisify(fs.unlink)(this.filename);
+    await fs.unlink(this.filename);
   }
 
   async stat() {
-    return await promisify(fs.stat)(this.filename);
+    return await fs.stat(this.filename);
   }
 
   async open(mode = 'r+') {
     this.logger.log('opening', this.filename);
     if (this.isOpen)
       throw new Error('File already open');
-    this.fd = await fsOpen(this.filename, mode);
-  }
-
-  openSync(mode = 'r+') {
-    if (this.isOpen)
-      throw new Error('File already open');
-    this.fd = fs.openSync(this.filename, mode);
+    this.file = await fs.open(this.filename, mode);
   }
 
   async close() {
     this.logger.log('closing', this.filename);
     this.logger.log('reads', this.reads);
     this.logger.log('writes', this.writes);
-    if (!this.fd)
+    if (!this.file)
       throw new Error('No open file to close');
-    await fsClose(this.fd);
-    this.fd = null;
+    await this.file.close();
+    this.file = null;
     this.reads = 0;
     this.writes = 0;
-  }
-
-  closeSync() {
-    if (!this.fd)
-      throw new Error('No open file to close');
-    fs.closeSync(this.fd);
   }
 
   async lock(pos = 0, options = { exclusive: false }) {
@@ -106,7 +86,7 @@ class File extends EventEmitter {
       ++lockedPosition.count;
       lockedPosition.exclusive = options.exclusive;
       this.lockedPositions.set(pos, lockedPosition);
-      await lock(this.fd!, pos, 1, options);
+      await lock(this.file!.fd, pos, 1, options);
       return;
     } else {
       const timeout = setInterval(() => { }, ~0 >>> 1);
@@ -121,7 +101,7 @@ class File extends EventEmitter {
   }
 
   async unlock(pos = 0) {
-    await unlock(this.fd!, pos, 1);
+    await unlock(this.file!.fd, pos, 1);
     const lockedPosition = this.lockedPositions.get(pos)!;
     lockedPosition.exclusive = false;
     --lockedPosition.count;
